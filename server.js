0a98ea14907af9d68adf22e8b71fc6559ec85c9c
Bryant Novas <bryant@MacBook-Air.local>
2016-11-20 01:46:30 -0500

    fixes node server

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 var path = require('path');
 
-var __projectRoot = __dirname + '/../../';
+var __projectRoot = __dirname ;
 
 app.get('/', function (req, res) {
     res.sendFile(path.join(__projectRoot + '/index.html'));
